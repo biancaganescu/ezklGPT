f6985d611d6bc0bb93bbaa2534eac20e9002c618
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-01 09:10:27 +0300

    added wget for kzg 7-12

---
+++ fyp_suite/wget_kzg_7_12.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+wget https://trusted-setup-halo2kzg.s3.eu-central-1.amazonaws.com/perpetual-powers-of-tau-raw-7
+
+mv perpetual-powers-of-tau-raw-7 kzg7.params
+
+wget https://trusted-setup-halo2kzg.s3.eu-central-1.amazonaws.com/perpetual-powers-of-tau-raw-8
+
+mv perpetual-powers-of-tau-raw-8 kzg8.params
+
+wget https://trusted-setup-halo2kzg.s3.eu-central-1.amazonaws.com/perpetual-powers-of-tau-raw-9
+
+mv perpetual-powers-of-tau-raw-9 kzg9.params
+
+wget https://trusted-setup-halo2kzg.s3.eu-central-1.amazonaws.com/perpetual-powers-of-tau-raw-10
+
+mv perpetual-powers-of-tau-raw-10 kzg10.params
+
+wget https://trusted-setup-halo2kzg.s3.eu-central-1.amazonaws.com/perpetual-powers-of-tau-raw-11
+
+mv perpetual-powers-of-tau-raw-11 kzg11.params
+
+wget https://trusted-setup-halo2kzg.s3.eu-central-1.amazonaws.com/perpetual-powers-of-tau-raw-12
+
+mv perpetual-powers-of-tau-raw-12 kzg12.params
