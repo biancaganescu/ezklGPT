6157ece5bb8a2802c0519725e60ae4d04b693139
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-03 14:15:26 +0300

    added tests for S1 and S2

---
+++ fyp_suite/nanoGPT/S1K14_S1K26.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/bash
+
+cd S1K14
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K16
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K18
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K20
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K22
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K24
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K26
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
