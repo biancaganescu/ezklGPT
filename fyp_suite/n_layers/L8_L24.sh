419af07d2197ecf3e57249fe831162d5bad209b5
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-05 11:37:27 +0300

    added number of layers tests

---
+++ fyp_suite/n_layers/L8_L24.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/bash
+
+cd L8
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L12
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L16
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L20
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L24
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+
