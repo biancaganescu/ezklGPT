a7eb64b46df43d496f83287aa282a17125a7e362
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-07 16:54:30 +0300

    added timestamps to S0 and S1 tests

---
+++ fyp_suite/nanoGPT/S1K16_S1K24.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/bash
+
+cd S1K16
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K18
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K20
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K22
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd S1K24
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
