b7c8f0d7a996e31d430f8e06749adf1c5b2eada1
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-07 23:28:04 +0300

    more tests

---
+++ fyp_suite/linear/L12_L61.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/bash
+
+cd L12
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L19
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L27
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L48
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd L61
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+
