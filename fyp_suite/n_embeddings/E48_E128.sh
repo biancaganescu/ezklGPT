a302b590f5b035c083fee2c89ade02398458ca2f
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-07 11:01:48 +0300

    added number of embeddings test

---
+++ fyp_suite/n_embeddings/E48_E128.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/bash
+
+cd E80
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd E96
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd E112
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd E128
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd E144
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
