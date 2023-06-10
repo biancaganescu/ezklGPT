0ecb7f092f19fb34c786d93c570dc6a6927d45fc
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-10 23:16:15 +0300

    added block size tests

---
+++ fyp_suite/block size/B80_B128.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/bash
+
+cd B80
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd B96
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd B112
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd B128
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
+
+cd B144
+
+sh run.sh
+
+sh delete.sh
+
+cd ..
