89632cace81b2e70d610e8168d597b2cfb669e28
Bianca-Mihaela Ganescu <biancaganescu@192-168-0-100.rdsnet.ro>
2023-06-11 13:13:18 +0300

    fixed run.sh in shakespeare

--- fyp_suite/trained_shakespeare_char/run.sh
+++ fyp_suite/trained_shakespeare_char/run.sh
@@ -8,16 +8,16 @@ RUST_LOG=debug cargo run --bin ezkl --release forward --data input.json --model 
 
 echo "SETUP START TIME $(date)" | tee -a logs/setup_log.txt
 
-RUST_LOG=debug cargo run --bin ezkl --release setup -D output.json -M network.onnx --params-path=../../kzg24.params --vk-path=vk.key --pk-path=pk.key --circuit-params-path=circuit.params -K=24 -S=0 -B=23  | tee -a logs/setup_log.txt
+RUST_LOG=debug cargo run --bin ezkl --release setup -D output.json -M network.onnx --params-path=../kzg24.params --vk-path=vk.key --pk-path=pk.key --circuit-params-path=circuit.params -K=24 -S=0 -B=23  | tee -a logs/setup_log.txt
 
 echo "SETUP END TIME $(date)" | tee -a logs/setup_log.txt
 
 echo "PROVE START TIME $(date)" | tee -a logs/prove_log.txt
 
-RUST_LOG=debug cargo run --bin ezkl --release  prove -M network.onnx -D output.json --pk-path=pk.key --proof-path=model.proof --params-path=../../kzg24.params --circuit-params-path=circuit.params | tee -a logs/prove_log.txt
+RUST_LOG=debug cargo run --bin ezkl --release  prove -M network.onnx -D output.json --pk-path=pk.key --proof-path=model.proof --params-path=../kzg24.params --circuit-params-path=circuit.params | tee -a logs/prove_log.txt
 
 echo "PROVE END TIME $(date)" | tee -a logs/prove_log.txt
 
-RUST_LOG=debug cargo run --bin ezkl --release verify --proof-path=model.proof --circuit-params-path=circuit.params --vk-path=vk.key --params-path=../../kzg24.params | tee logs/verify_log.txt
+RUST_LOG=debug cargo run --bin ezkl --release verify --proof-path=model.proof --circuit-params-path=circuit.params --vk-path=vk.key --params-path=../kzg24.params | tee logs/verify_log.txt
 
 ls -l | tee logs/sizes_log.txt
